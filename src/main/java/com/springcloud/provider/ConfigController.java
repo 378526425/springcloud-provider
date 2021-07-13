9283b854b4828c52b50a21dba70c9bce15315edd
sc_wanglei <378526425@qq.com>
2021-07-13 11:34:07 +0800

    nacos配置文件

--- src/main/java/com/springcloud/provider/ConfigController.java
+++ src/main/java/com/springcloud/provider/ConfigController.java
@@ -2,24 +2,26 @@ package com.springcloud.provider;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @program: provider
  * @description:
  * @author: Mr.Wang
- * @create: 2021-06-17 16:36
+ * @create: 2021-06-18 11:35
  **/
 
 @RestController
+@RequestMapping("/config")
 @RefreshScope
-public class HelloController {
-    @Value("${username}")
-    private String port;
+public class ConfigController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello Nacos Discovery "+this.port;
-    }
+    /*@Value("${useLocalCache:false}")
+    private boolean useLocalCache;
+
+    @RequestMapping("/get")
+    public boolean get() {
+        return useLocalCache;
+    }*/
 }
